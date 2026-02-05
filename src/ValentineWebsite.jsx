b009661b0c494ab0a842d17f7f305581e6bbbdc8
saji <mshadab9810@gmail.com>
2026-02-05 12:22:07 +0530

    Update ValentineWebsite.jsx

--- src/ValentineWebsite.jsx
+++ src/ValentineWebsite.jsx
@@ -48,7 +48,7 @@ const ValentineWebsite = () => {
     { id: 1, caption: 'Best Bady 🌙', image: 'https://ik.imagekit.io/fnqps1ql6/New%20Folder/WhatsApp%20Image%202026-02-05%20at%209.03.10%20AM%20(1).jpeg' },
     { id: 2, caption: 'That unforgettable surprise', image: 'https://ik.imagekit.io/fnqps1ql6/New%20Folder/WhatsApp%20Image%202026-02-05%20at%209.03.09%20AM.jpeg' },
     { id: 3, caption: 'Forever 💝', image: 'https://ik.imagekit.io/fnqps1ql6/New%20Folder/WhatsApp%20Image%202026-02-05%20at%209.03.09%20AM%20(1).jpeg' },
-    { id: 4, caption: 'https://ik.imagekit.io/fnqps1ql6/New%20Folder/WhatsApp%20Image%202026-02-05%20at%209.03.10%20AM.jpeg' },
+    { id: 4, caption: 'Smile🌸', image:'https://ik.imagekit.io/fnqps1ql6/New%20Folder/WhatsApp%20Image%202026-02-05%20at%209.03.10%20AM.jpeg'  },
     // { id: 5, caption: 'Our cozy evenings', image: 'PASTE_YOUR_IMAGE_URL_HERE' },
     // { id: 6, caption: 'Making memories', image: 'PASTE_YOUR_IMAGE_URL_HERE' }
   ];
